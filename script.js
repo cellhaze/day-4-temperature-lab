d54b1e6d45e9cd5250ed21363a6748abda2c2293
Marcell Hayes <marcell.made@gmail.com>
2017-04-13 13:30:26 -0400

    initial push. class exercise still included on top.

---
+++ script.js
@@ -0,0 +1,19 @@
+var points = 7;
+
+var percent = points / 10 * 100;
+
+console.log("Score: " + percent + "%" );
+
+if (percent >= 60) {
+    console.log("That's a passing score!");
+} else {
+    console.log("Try again");
+}
+
+
+var temperature = 80;
+    
+var celsius = (temperature - 32) * .556;
+
+console.log("If it is " + temperature + "degress fahrenheit,")
+console.log("it is " + celsius + " degrees celsius.");
